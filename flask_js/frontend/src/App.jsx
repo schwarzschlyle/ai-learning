7f531ad58e02e0bddad5661a49634b7e4736d62e
Lyle Kenneth Geraldez <lyleken@MacBook-Air.local>
2024-12-05 17:01:46 +0800

    build frontend using react

---
+++ flask_js/frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import ContactList from './ContactList'
+import './App.css'
+import ContactForm from './ContactForm'
+
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [ isModalOpen, setIsModalOpen ] = useState(false)
+
+  useEffect(() => {
+    fetchContacts()
+    
+  }, [])
+
+
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+
+
+  }
+
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  return (<>
+  <ContactList contacts = {contacts} />
+  <button onClick={openCreateModal}> Create New Contact </button>
+  { isModalOpen && <div className="modal">
+  <div className="modal-content">
+    <span className="close" onClick={closeModal}>&times;</span>
+    <ContactForm />
+  </div>
+  </div>
+
+  }
+    
+    </>
+  );
+
+}
+
+
+
+
+export default App;
